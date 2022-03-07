8936db777d91ee4af70779ef05559ab4ae53afe8
RaminGhaffari <raminghafary@gmail.com>
2022-03-07 22:35:02 +0330

    Add Document And Demo WebPage

---
+++ Docs/Models/paginateddto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Docs.Models
+{
+    public class paginateddto
+    {
+        public int totalrow { get; set; }
+        public int pagesize { get; set; }
+        public int pageindex { get; set; }
+        public IEnumerable<human> Humen { get; set; }
+
+    }
+}
